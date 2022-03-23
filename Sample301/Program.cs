afbb96d767650e759a0d0ab7d846eda8117db58e
DongHeon Lee <niche3600@gmail.com>
2022-03-23 12:43:41 +0900

    for example

--- Sample301/Program.cs
+++ Sample301/Program.cs
@@ -1,3 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+int[] intArray = new int[] { 1, 2, 3, 4, 5 };
+
+// 基本的なfor
+
+for (int i = 1; i <= 5; i++) {
+    Console.Write(i + " ");
+}
+Console.WriteLine();
+
+// 基本的なforeach
+
+foreach (int num in intArray) {
+    Console.Write(num + " ");
+}
+
+Console.WriteLine();
+Console.WriteLine();
+
+// 多重ループ
+
+for (int i = 1; i <= 2; i++) {
+    for (int j = 1; j <= 3; j++) {
+        int k = i + j;
+        Console.WriteLine("{0} + {1} = {2}", i, j, k);
+    }
+    Console.WriteLine();
+}
+
+
+// while
 
