5952bb09ba0406f4fc6efd1ad01bd2726ebd7bf9
DongHeon Lee <niche3600@gmail.com>
2022-03-24 08:53:26 +0900

    array example

---
+++ Sample_3_5_example/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("救急の表示");
+
+for (int i = 1; i <= 9; i++) {
+    for (int j = 1; j <= 9; j++)
+    {
+        Console.Write("{0} ✕ {1} = {2,2:d} ", i, j, i*j);
+    }
+    Console.WriteLine();
+}
