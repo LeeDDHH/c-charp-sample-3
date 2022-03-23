bfdc760b6582e7f276bfbd74cb8d126ba1948d22
DongHeon Lee <niche3600@gmail.com>
2022-03-24 08:14:14 +0900

    max min value example

---
+++ Sample_3_4_example/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random rnd = new Random();
+
+int max = 0;
+int min = 101;
+int count = 1;
+
+Console.WriteLine("1から100の間の数をランダムに10回生成する");
+Console.WriteLine("最大値と最小値を表示する");
+
+while(count <= 10)
+{
+    int rndValue = rnd.Next(1, 101);
+    Console.Write($"{rndValue} ");
+
+    if (rndValue > max) {
+        max = rndValue;
+    }
+    if (rndValue < min) {
+        min = rndValue;
+    }
+    count++;
+};
+
+if (count >= 10) {
+    Console.WriteLine();
+
+    Console.WriteLine($"最大値：{max}");
+    Console.WriteLine($"最小値：{min}");
+};
