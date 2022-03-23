0f23d1987cb409ff0277568577faf5d8c711ea85
DongHeon Lee <niche3600@gmail.com>
2022-03-23 13:29:16 +0900

    loop example with try catch example

---
+++ Sample3_1_example/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int num;
+
+Console.Write("★の数を入力：");
+try {
+    num = int.Parse(Console.ReadLine());
+} catch(Exception e) {
+    Console.WriteLine($"ERROR:{e.Message}");
+    return;
+}
+
+// for
+
+for (int i = 0; i < num; i++) {
+    Console.Write("★");
+}
+Console.WriteLine();
+
+// while
+
+int j = 0;
+while (j < num) {
+    Console.Write("★");
+    j++;
+}
+Console.WriteLine();
+
+// do while
+
+int k = 0;
+do
+{
+    Console.Write("★");
+    k++;
+}
+while (k < num);
+Console.WriteLine();
