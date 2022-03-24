1aa31cab35151d3014a623d490c27e8ec7178ed6
DongHeon Lee <niche3600@gmail.com>
2022-03-24 09:20:42 +0900

    array foreach with index and value example

--- Sample308/Program.cs
+++ Sample308/Program.cs
@@ -2,6 +2,48 @@
 
 int[] n = { 1, 2, 3, 4 };
 
+// 基本型
+
+Console.WriteLine("基本型");
+
 foreach (int i in n) {
     Console.Write($"{i} ");
+}
+
+Console.WriteLine();
+
+// 値とインデックスを取得する方法
+// C# 7.0以降で使えるタプル(tuple)を使った方法
+
+Console.WriteLine("タプル(tuple)を使った方法");
+
+foreach (var (value, index) in n.Select((value, index) => (value, index)))
+{
+    Console.WriteLine($"index={index} value={value}");
+}
+
+Console.WriteLine();
+
+// 匿名型を使った方法
+
+Console.WriteLine("匿名型を使った方法");
+
+foreach (var item in n.Select((value, index) => new { value, index }))
+{
+    Console.WriteLine($"index={item.index} value={item.value}");
+}
+
+Console.WriteLine();
+
+// index用の変数を使った方法
+
+Console.WriteLine("index用の変数を使った方法");
+
+int indexForArray = -1;
+
+foreach (var value in n) {
+
+    // continue を考慮し、必ずループの先頭でインクリメント
+    indexForArray += 1;
+    Console.WriteLine($"index={indexForArray} value={value}");
 }
