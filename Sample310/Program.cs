202f06e14c0817f6930eafc24de19b5ba18836d4
DongHeon Lee <niche3600@gmail.com>
2022-03-24 10:26:53 +0900

    jaggedArray example

---
+++ Sample310/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[][] a = new int[][] {
+    new int[] { 0, 1 },
+    new int[] { 2 },
+    new int[] { 3, 4, 5, 6 }
+};
+
+for (int m = 0; m < a.Length; m++) {
+    for (int n = 0; n < a[m].Length; n++) {
+        Console.Write($"{m}番目の配列の中の{n}番目の要素：{a[m][n]} ");
+    }
+    Console.WriteLine();
+}
