fefb4eaa6c952c698533ebb1df73e53d36379dd1
DongHeon Lee <niche3600@gmail.com>
2022-03-24 11:54:07 +0900

    generate random number array and display 10 units

---
+++ Sample_3_9_example/Program.cs
@@ -0,0 +1,24 @@
+﻿Random rnd = new Random();
+
+int[] rndIntArray = new int[10];
+int index = -1;
+
+foreach (int i in rndIntArray)
+{
+    index++;
+    rndIntArray[index] = rnd.Next(1, 100);
+    Console.Write($"{rndIntArray[index]} ");
+}
+
+Console.WriteLine();
+
+for (int i = 0; i <= 100; i += 10) {
+    Console.Write($"{i}以上{i+10}未満： ");
+
+    foreach (int n in rndIntArray) {
+        if (n > i && n < i + 10) {
+            Console.Write($"{n} ");
+        }
+    }
+    Console.WriteLine();
+}
