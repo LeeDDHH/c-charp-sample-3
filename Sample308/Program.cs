53630e14969300333a80db79e4d83520a786595a
DongHeon Lee <niche3600@gmail.com>
2022-03-24 09:11:16 +0900

    array foreach example

--- Sample308/Program.cs
+++ Sample308/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
+int[] n = { 1, 2, 3, 4 };
+
+foreach (int i in n) {
+    Console.Write($"{i} ");
+}
