6f0a5d7556d85b203096667cace326fb7515229c
DongHeon Lee <niche3600@gmail.com>
2022-03-24 09:08:11 +0900

    array example

---
+++ Sample307/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int[] n = { 5, 4, 3, 2, 1 };
+string[] s = { "ABC", "DEF", "GHI" };
+
+int i = 0;
+
+for (i = 0; i < n.Length; i++) {
+    Console.Write($"{n[i]} ");
+}
+
+Console.WriteLine();
+
+for (i = 0; i < s.Length; i++)
+{
+    Console.Write($"{s[i]} ");
+}
