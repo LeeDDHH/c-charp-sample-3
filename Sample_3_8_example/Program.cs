a366401b48be99252981f87b8b135623f4a2a527
DongHeon Lee <niche3600@gmail.com>
2022-03-24 11:27:56 +0900

    generate random number array and sort

---
+++ Sample_3_8_example/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Random rnd = new Random();
+
+int[] rndIntArray = new int[10];
+int index = -1;
+
+foreach (int i in rndIntArray) {
+    index++;
+    rndIntArray[index] = rnd.Next(1,101);
+    Console.Write($"{rndIntArray[index]} ");
+}
+
+Console.WriteLine();
+
+Array.Sort(rndIntArray);
+
+foreach (int i in rndIntArray)
+{
+    Console.Write($"{i} ");
+}
